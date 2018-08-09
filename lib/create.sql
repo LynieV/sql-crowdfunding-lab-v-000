e1c557bdee2e1427eb0fe4ecb82491be35944751
Valerie Lynie McClimon <vmcclimon33@yahoo.com>
2018-08-09 03:23:21 +0000

    automatically backed up by learn

--- lib/create.sql
+++ lib/create.sql
@@ -5,4 +5,17 @@ CREATE TABLE projects (
   funding_goal INTEGER,
   start_date INTEGER,
   end_date INTEGER
-)
+);
+
+CREATE TABLE users (
+  id INTEGER PRIMARY KEY,
+  name TEXT,
+  age INTEGER
+);
+
+CREATE TABLE pledges (
+  id INTEGER PRIMARY KEY,
+  amount INTEGER,
+  user_id INTEGER,
+  project_id INTEGER
+);
